2b55b2554bc47f8bc9a849b97dc3f698fd054a1a
Infinityvy <benoo@gmx.de>
2024-09-29 02:01:53 +0200

    Added experimental flashlight controls

--- Assets/Scripts/Player/Flashlight.cs
+++ Assets/Scripts/Player/Flashlight.cs
@@ -4,21 +4,22 @@ namespace Player
 {
     public class Flashlight : MonoBehaviour
     {
-        public static Flashlight instance;
+        public Camera mainCamera;
+        public Transform test;
 
         private new Light light;
 
         private void Start()
         {
             light = GetComponent<Light>();
-            instance = this;
         }
 
         private void Update()
         {
             if(Session.instance.paused) return;
 
-            rotateFlashlightToCursor();
+            //rotateFlashlightToCursor();
+            lookAtCursor();
             toggleFlashlight();
         }
 
@@ -35,6 +36,20 @@ namespace Player
             float angle = GameUtility.getCursorAngleRelativeToPlayer();
 
             transform.rotation = Quaternion.Euler(30, angle, 0f);
+        }
+
+        private void lookAtCursor()
+        {
+            Ray ray = mainCamera.ScreenPointToRay(GameUtility.clampMousePosition(Input.mousePosition));
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Vector3 direction = hit.point - transform.position;
+                test.position = hit.point;
+                //direction.y = 0; // Optional: keep the object level on the y-axis
+                transform.forward = direction.normalized;
+            }
         }
     }
 }
